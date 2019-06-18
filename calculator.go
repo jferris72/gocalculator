2b97b1856b0baca8ef4f8cc029eb4823450fdf96
Justin Ferris <justin.ferris@workday.com>
2019-06-18 13:15:06 -0700

    Check if word is number and print it

--- calculator.go
+++ calculator.go
@@ -4,11 +4,31 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ")
+	fmt.Print("Let's calculate something \n")
 	text, _ := reader.ReadString('\n')
-	fmt.Println(text)
+	calculate(strings.Fields(text))
+}
+
+func calculate(words []string) {
+	var firstNum int64
+	for _, word := range words {
+		if checkIsNumber(word) {
+			firstNum, _ = strconv.ParseInt(word, 10, 64)
+			fmt.Print(firstNum)
+			fmt.Print("\n")
+		}
+	}
+}
+
+func checkIsNumber(word string) bool {
+	if _, err := strconv.ParseInt(word, 10, 64); err == nil {
+		return true
+	}
+	return false
 }
